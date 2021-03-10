cb5e1bd00c8fd97da2e958bb622e981e7568bdaa
Shami Tomita <shamitomita@gmail.com>
2021-03-10 00:02:07 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
     def move(board)
       if board.cells[4] == " "
         "5"
-        elsif board.cells[0] == " "
+        elsif board.cells[0] == " " && board.cells[2] == " "
         "1"
         elsif board.cells[2] == " "
         "3"
